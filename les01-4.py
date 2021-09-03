8f4ae043c3946ec050f1b1ecc4059c3d87bddd7e
Anna Bagdinova <ANB265@yandex.ru>
2021-09-03 16:49:13 +0300

    Changes in task 4 lesson 1

--- les01-4.py
+++ les01-4.py
@@ -1,13 +1,14 @@
 number = list(input('Введите число: '))
 int_number = [int(x) for x in number]
-max_num = 0
-i = 0
+max_num = 1
+i = len(int_number)
 
 print(i)
-while max_num < int_number[i]:
-    i+=1
-    max_num = int_number[i]
+while i > 0:
+    i -= 1
+
+    while max_num < int_number[i]:
+        max_num = int_number[i]
 
 print(max_num)
-
 
