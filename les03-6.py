11dd535c306e175e15a66c75f7376c5278d49c48
Anna Bagdinova <ANB265@yandex.ru>
2021-09-13 15:49:32 +0300

    Добавлены 5 и 6 задания

--- les03-6.py
+++ les03-6.py
@@ -2,7 +2,7 @@ def int_func(low_string):
     titled_string = low_string.title()
     print(titled_string)
 
-#string = 'to be or not to be'
+# string = 'to be or not to be'
 string = input('Введите слова через пробел: ')
 
 int_func(string)
