3990004cf271f9776506695dc307a35de3279948
Anb265 <89507497+Anb265@users.noreply.github.com>
2021-09-27 19:57:55 +0300

    Update les04-5.py

--- les04-5.py
+++ les04-5.py
@@ -6,4 +6,4 @@ from functools import reduce
 #
 # print(reduce(multi, new_list))
 
-print(reduce(lambda a,b:a*b, [i for i in range(100, 1001, 2)]))
+print(reduce(lambda a, b: a * b, [i for i in range(100, 1001, 2)]))
